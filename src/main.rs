83eee0d5aaf422f4d05a22bab49cd1a4a001229c
Icebound777 <frozenblue@gmx.de>
2023-11-11 00:22:36 +0100

    feat: check provided ROM sha1 checksum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+// Sha1 checksum calc
+use hex_literal::hex;
+use sha1::{Sha1, Digest};
+
+// OS file access
+use std::env;
+use std::fs;
+
+const BT_US_10_ROM_SHA1: [u8; 20] = hex!("af1a89e12b638b8d82cc4c085c8e01d4cba03fb3");
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let rom_file_path = &args[1];
+
+    if check_rom_checksum(rom_file_path.clone()) {
+        println!("Yay");
+    } else {
+        println!("Nay");
+    }
+}
+
+fn check_rom_checksum(file_path: String) -> bool {
+    let rom_content = fs::read(file_path)
+        .expect("Could not read provided file");
+
+    let mut hasher = Sha1::new();
+
+    hasher.update(rom_content);
+
+    let result = hasher.finalize();
+
+    result[..] == BT_US_10_ROM_SHA1
 }
